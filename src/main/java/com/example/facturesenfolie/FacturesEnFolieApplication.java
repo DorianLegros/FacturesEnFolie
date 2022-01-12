e85570635c1083bb72391e0f9e7d8e8767251b23
Dorian Nope <d.ratha76@hotmail.com>
2022-01-12 16:23:20 +0100

    Initial commit

---
+++ src/main/java/com/example/facturesenfolie/FacturesEnFolieApplication.java
@@ -0,0 +1,13 @@
+package com.example.facturesenfolie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturesEnFolieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturesEnFolieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/facturesenfolie/FacturesEnFolieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facturesenfolie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturesEnFolieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
